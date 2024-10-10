b38cc3b7616a8ad1b06292381a3dbd6863121646
Sandra Ortiz <elizabethortizr16@gmail.com>
2024-10-10 13:52:01 -0500

    ashlyandsandra

---
+++ juegos.py
@@ -0,0 +1,30 @@
+def player_1():
+    val = input ("Choice: rock, paper or scissors.")
+
+    if val == "rock":
+        print("You winner!!:)")
+    else:
+        print("You winner!!:)")
+
+def player_2():
+    val = input()
+    if val == "paper":
+        print("You winner!!:)")
+    else:
+        print("You lose:(!!")
+
+def player1():
+    val = input ("Choice: rock, paper or scissors.")
+    if val == input:
+        print(":")
+
+
+
+
+
+
+if __name__== "__main__":
+    player1()
+    player_2()
+ 
+ 
