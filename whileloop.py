cfbf9c6bde013125097a0b57ee7ef793f6f0794f
Sandra <elizabethortizr16@gmail.com>
2024-11-12 13:57:43 -0600

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,15 @@
+total = 0
+count = 0
+
+userInput= input("Please enter a number:")
+
+while userInput != "done":
+    userInput= input("Please enter a number:")
+    count =+ 1
+    if userInput !="done":
+        total += int(userInput)
+       
+   
+
+
+print("Running overage:", total / count)
